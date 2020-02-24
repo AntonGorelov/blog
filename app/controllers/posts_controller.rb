580f32f4e45e44e8af52be4759483e674642b387
AntonG <ant7016@yandex.ru>
2020-02-24 21:01:06 +0300

    Add create, update, destroy to posts controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,32 @@ class PostsController < ApplicationController
         render json: @post
     end
 
+    def create
+        @post = Post.new(post_params)
+
+        if @post.save
+            render json: @post, status: :created, location: @post
+        else
+            render json: @post.errors, status: :unprocessable_entity
+    end
+
+    def update
+        if @post.update(post_params)
+            render json: @post
+        else
+            render json: @post.errors, status: :unprocessable_entity
+        end
+
+    def destroy
+        @post.destroy
+    end
+
     private
         def set_post
             @post = Post.find(params[:id])
+        end
+
+        def post_params()
+            params.require(:post).permit!
         end
 end
